562523b1506e478f91b37def480a103495b25778
MoCHIxb <154912745+MoCHIxb@users.noreply.github.com>
2024-03-26 18:21:23 +0800

    v1.1 try1.py
    添加排名系统

--- try1.py
+++ try1.py
@@ -48,13 +48,46 @@ def login():
     password = request.form.get('password')
     res = get_user(username, password)
     if res:
-        return render_template('main.html',msg='登陆成功')
+        return render_template('main.html',msg='登陆成功')############
     else:
-        return render_template('login.html', msg='登录失败')
+        return render_template('login.html', msg='登录失败')#############
 """
 @app.route('/register',methods=['GET','POST'])
 def register():
     
 """
+
+@app.route('/list',methods=['GET','POST'])
+def list():
+    sql = "select * from users"
+    data=query(sql)
+    db1,cursor1=get_db()
+    if request.method == "POST":
+        Name=request.form.get('Name',None)
+        Points = request.form.get('Points',None)
+        sql = "select id from users where username= '" + str(Name) + "'"
+        res=query(sql)
+        if res:
+            insert = request.values.get("插入")
+            sort_button = request.values.get("排序")
+            print(Name,Points)
+            if insert=='insert':
+                if Name and Points:
+                    insert_sql = "update users set point=" +str(Point+res[0][6])+" where username= '"+str(Name)+"'"##########
+                    cursor1.execute(insert_sql)
+                    db1.commit()
+            if sort_button=='sort':
+                sort_sql = "select * from users order by points desc limit 10"
+                cursor1.execute(sort_sql)
+                data = cursor1.fetchall()
+        else:
+            data='不是注册用户！'
+
+    print(data)
+
+    return render_template("list.html",data=data)
+
+
+
 if __name__== '__main__':
     app.run()
